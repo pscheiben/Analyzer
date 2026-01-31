76398d69ad56f4d4a080dd568de59ef14997624d
Peter Scheibenhoffer <peter.scheibenhoffer@gmail.com>
2026-01-31 22:34:18 +0000

    almost nothing

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ import tkinter as tk            # Import the tkinter module as tk
 
 if __name__ == "__main__":      # If the script is being run as the main program
     root = tk.Tk()              # Create a new Tkinter root window
-    app = AnalyzerApp(root)  # Create a new instance of the CSVAnalyzerApp class
+    app = AnalyzerApp(root)     # Create a new instance of the CSVAnalyzerApp class
     root.mainloop()             # Start the Tkinter main event loop
